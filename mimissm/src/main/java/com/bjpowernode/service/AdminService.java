a5ef4ff41d5420a08cdd78cce32745e4813a226d
CrazyprogrammerKim <58496577+CrazyprogrammerKim@users.noreply.github.com>
2022-04-25 14:32:37 +0800

    Second submit
    Second submit ,FIshWeb

--- mimissm/src/main/java/com/bjpowernode/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.bjpowernode.service;
-
-import com.bjpowernode.pojo.Admin;
-
-public interface AdminService {
-    //完成登录判断
-    Admin login(String name, String pwd);
- 
-}
--- mimissm/src/main/java/com/bjpowernode/service/ProductTypeService.java
+++
@@ -1,9 +0,0 @@
-package com.bjpowernode.service;
-
-import com.bjpowernode.pojo.ProductType;
-
-import java.util.List;
-
-public interface ProductTypeService {
-    List<ProductType> getAll();
-}
